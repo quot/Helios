3662a4b75c586b7599049d5d6b4641ac14ec4d1d
Alex Coté <alex.cote@tuta.io>
2025-05-30 12:59:52 -0500

    Fix view mode; Add command logging and note

--- plugin.py
+++ plugin.py
@@ -13,7 +13,7 @@ import sublime_plugin as splug
 
 logger = logging.getLogger(__package__)
 logger.propagate = False
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # Avoid duplicate loggers e.g., if the plugin is reloaded.
 if not logger.hasHandlers():
@@ -33,14 +33,21 @@ class Mode(enum.Enum):
     NORMAL = enum.auto()
     INSERT = enum.auto()
     SELECT = enum.auto()
-    MINOR_VIEW = enum.auto()
+    VIEW = enum.auto()
     # SPACE = enum.auto()
     # GOTO = enum.auto()
     # MATCH = enum.auto()
     # COMMAND = enum.auto()
     # WINDOW = enum.auto()
 
-minorModes: List[Mode] = [Mode.MINOR_VIEW]
+minorModes: List[Mode] = [
+    Mode.VIEW,
+    # Mode.SPACE,
+    # Mode.GOTO,
+    # Mode.MATCH,
+    # Mode.COMMAND,
+    # Mode.WINDOW,
+]
 
 #############
 ## Globals ##
@@ -52,7 +59,7 @@ settings = {
     Mode.NORMAL: Settings(caretBlock=True),
     Mode.INSERT: Settings(caretBlock=False),
     Mode.SELECT: Settings(caretBlock=True),
-    Mode.MINOR_VIEW: Settings(caretBlock=True),
+    Mode.VIEW: Settings(caretBlock=True),
 }
 
 state = {
@@ -159,7 +166,7 @@ def plugin_unloaded():
 
 class HelixEventListener(splug.EventListener):
     def on_query_context(self, view: subl.View, contextKey: str, operator: subl.QueryOperator, operand: str, match_all: bool):
-        logger.debug(f"on_query_context triggered! Query: key [{contextKey}], operator [{operator}], operand [{operand}]")
+        # logger.debug(f"on_query_context triggered! Query: key [{contextKey}], operator [{operator}], operand [{operand}]")
         global state
         if (state["pluginEnabled"] and contextKey  == "helix_mode"):
             if (operator == subl.QueryOperator.EQUAL):
@@ -172,35 +179,31 @@ class HelixEventListener(splug.EventListener):
             return False
 
     def on_activated(self, view):
-        logger.debug("on_activated triggered")
-        if (is_view(view)):
-            logger.debug(f"is_view: True")
-            set_mode(Mode.NORMAL, view=view)
-        else:
-            logger.debug(f"is_view: False -- Disabling helix!")
-            global state
-            state["pluginEnabled"] = False
+        set_mode(Mode.NORMAL, view=view)
+        global state
+        state["pluginEnabled"] = is_view(view)
+        logger.debug(f"on_activated triggered! PluginEnabled: {state['pluginEnabled']}")
 
-    def on_text_command(self, view, command: str, args: dict):
-        logger.debug("on_text_command triggered")
+    # def on_text_command(self, view, command: str, args: dict):
+    #     logger.debug("on_text_command triggered")
 
-    def on_post_text_command(self, view, command, args):
-        logger.debug("on_post_text_command triggered")
+    # def on_post_text_command(self, view, command, args):
+    #     logger.debug("on_post_text_command triggered")
 
-    def on_load(self, view):
-        logger.debug("on_load triggered")
+    # def on_load(self, view):
+    #     logger.debug("on_load triggered")
 
-    def on_post_save(self, view):
-        logger.debug("on_post_save triggered")
+    # def on_post_save(self, view):
+    #     logger.debug("on_post_save triggered")
 
-    def on_close(self, view):
-        logger.debug("on_close triggered")
+    # def on_close(self, view):
+    #     logger.debug("on_close triggered")
 
-    def on_deactivated(self, view):
-        logger.debug("on_deactivated triggered")
+    # def on_deactivated(self, view):
+    #     logger.debug("on_deactivated triggered")
 
-    def on_exit(self):
-        logger.debug("on_exit triggered")
+    # def on_exit(self):
+    #     logger.debug("on_exit triggered")
 
 ####################
 ## Helix Commands ##
@@ -211,70 +214,84 @@ class HelixEventListener(splug.EventListener):
 ## Mode Switch
 class InsertModeCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("InsertModeCommand triggered")
         set_mode(Mode.INSERT, view = self.view)
 
 class NormalModeCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("NormalModeCommand triggered")
         set_mode(Mode.NORMAL, view = self.view)
 
 class SelectModeCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("SelectModeCommand triggered")
         set_mode(Mode.SELECT, view = self.view)
 
 class ViewModeCommand(splug.TextCommand):
     def run(self, edit):
-        set_mode(Mode.MINOR_VIEW, view = self.view)
+        logger.debug("ViewModeCommand triggered")
+        set_mode(Mode.VIEW, view = self.view)
 
 class SpaceModeCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("SpaceModeCommand triggered")
         pass
 
 ##############
 ## Movement
 
+# FIX: Moving left and right on single character doesn't move the caret
 class MoveCharLeftCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MoveCharLeftCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "characters", "forward": False, "word_begin": False, "empty_line": False, "punct_begin": False, "separators": False, "extend": (state["mode"] == Mode.SELECT)})
 
 class MoveCharRightCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MoveCharRightCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "characters", "forward": True, "word_begin": False, "empty_line": False, "punct_begin": False, "separators": False, "extend": (state["mode"] == Mode.SELECT)})
 
 class MoveVisualLineUpCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MoveVisualLineUpCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "lines", "forward": False, "word_begin": False, "empty_line": False, "punct_begin": False, "separators": False, "extend": (state["mode"] == Mode.SELECT)})
 
 class MoveVisualLineDownCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MoveVisualLineDownCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "lines", "forward": True, "word_begin": False, "empty_line": False, "punct_begin": False, "separators": False, "extend": (state["mode"] == Mode.SELECT)})
 
 class MoveNextWordStartCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MoveNextWordStartCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "words", "forward": True, "word_begin": True, "empty_line": False, "punct_begin": False, "separators": False, "extend": True})
 
 class MovePrevWordStartCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("MovePrevWordStartCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         self.view.run_command(cmd="move", args={"by": "words", "forward": False, "word_begin": True, "empty_line": False, "punct_begin": False, "separators": False, "extend": True})
 
 class GotoLineStartCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("GotoLineStartCommand triggered")
         global state
         self.view.run_command(cmd="move_to", args={"to": "bol", "extend": (state["mode"] == Mode.SELECT)})
 
 class GotoLineEndCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("GotoLineEndCommand triggered")
         global state
         self.view.run_command(cmd="move_to", args={"to": "eol", "extend": (state["mode"] == Mode.SELECT)})
 
@@ -283,28 +300,33 @@ class GotoLineEndCommand(splug.TextCommand):
 
 class DeleteSelectionCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("DeleteSelectionCommand triggered")
         for region in self.view.sel():
             self.view.erase(edit, region)
 
 class ChangeSelectionCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("ChangeSelectionCommand triggered")
         for region in self.view.sel():
             self.view.erase(edit, region)
         set_mode(Mode.INSERT, self.view)
 
 class InsertAtLineEndCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("InsertAtLineEndCommand triggered")
         self.view.run_command(cmd="move_to", args={"to": "eol"})
         set_mode(Mode.INSERT, self.view)
 
 class OpenBelowCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("OpenBelowCommand triggered")
         self.view.run_command(cmd="move_to", args={"to": "eol"})
         self.view.run_command(cmd="insert", args={"characters": "\n"})
         set_mode(Mode.INSERT, self.view)
 
 class OpenAboveCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("OpenAboveCommand triggered")
         self.view.run_command(cmd="move_to", args={"to": "bol"})
         self.view.run_command(cmd="insert", args={"characters": "\n"})
         self.view.run_command(cmd="move", args={"by": "lines", "forward": False})
@@ -312,6 +334,7 @@ class OpenAboveCommand(splug.TextCommand):
 
 class GotoFileStartCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("GotoFileStartCommand triggered")
         self.view.run_command(cmd="move_to", args={"to": "bof"})
 
 ###########################
@@ -319,12 +342,12 @@ class GotoFileStartCommand(splug.TextCommand):
 
 class GotoNextParagraphCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("GotoNextParagraphCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         newSelections: List[subl.Region] = []
         for curSel in self.view.sel():
             nextParPoint = self.view.find(pattern="^\n[^\n]", start_pt=(curSel.b+1)).end()-1
-            logger.debug(nextParPoint)
             if (nextParPoint < 0): nextParPoint = len(self.view)
             if (nextParPoint == curSel.end()):
                 newSelections.append(subl.Region(a=nextParPoint, b=nextParPoint, xpos=curSel.xpos))
@@ -336,12 +359,12 @@ class GotoNextParagraphCommand(splug.TextCommand):
 
 class GotoPrevParagraphCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug("GotoPrevParagraphCommand triggered")
         global state
         if (state["mode"] == Mode.NORMAL): clear_selected(self.view)
         newSelections: List[subl.Region] = []
         for curSel in self.view.sel():
             nextParPoint = self.view.find(pattern="^\n[^\n]", start_pt=(curSel.b-1), flags=subl.FindFlags.REVERSE).begin()+1
-            logger.debug(nextParPoint)
             if (nextParPoint < 0): nextParPoint = 0
             if (nextParPoint == curSel.begin()):
                 newSelections.append(subl.Region(a=nextParPoint, b=nextParPoint, xpos=curSel.xpos))
@@ -357,6 +380,9 @@ class GotoPrevParagraphCommand(splug.TextCommand):
 
 class AlignViewCenterCommand(splug.TextCommand):
     def run(self, edit):
+        logger.debug(f"AlignViewCenterCommand triggered")
         if (len(self.view.sel()) > 0):
             self.view.show_at_center(self.view.sel()[0])
+        else:
+            logger.info(f"AlignViewCenterCommand called with {len(self.view.sel())} selections!")
         set_mode(Mode.NORMAL, self.view)
